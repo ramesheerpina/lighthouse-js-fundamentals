c3053c7334ca479da135c03eeeacc8c44295c81d
ramesheerpina <ramesh.eerpina@gmail.com>
2019-06-06 03:19:59 +0000

    another example of while loops

--- loops.js
+++ loops.js
@@ -1,13 +1,18 @@
-number = 1;
-while (number < 100) {
-  if (number%3 == 0 && number%5 ==0) {
-    console.log("FizzBuzz");
-  } else if (number%5 === 0) {
-    console.log("Buzz");
-  } else if (number%3 === 0) {
-    console.log ("Fizz");
-  } else {
-    console.log(number);
-  }
-  number = number+1;
+var num = 99;
+var line = '';
+var newLine = '';
+
+while (num <= 99 && num >= 0) {
+    if (num !== 1 && num !==0) {
+        line = (num+" bottles of juice on the wall! " + num +" bottles of juice! Take one down, pass it around... " );
+        newLine =newLine+line;
+    } else if (num===1) {
+        line2 = (num+" bottle of juice on the wall! " + num +" bottle of juice! Take one down, pass it around... " );
+        newLine = newLine+line2;
+    } else {
+        line4 = (num+" bottles of juice on the wall!" );
+        newLine = newLine+line4;
+    }
+    num = num-1;
 }
+console.log(newLine);
