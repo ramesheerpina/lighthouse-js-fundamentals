bc182659aa1d92ce993ceb2f8365d13729a54857
ramesheerpina <ramesh.eerpina@gmail.com>
2019-06-11 15:44:22 +0000

    made some fomrating changes to the text

--- howManyHundreds.js
+++ howManyHundreds.js
@@ -1,5 +1,5 @@
 function howManyHundreds(value) {
-  reminder = value%100;
-  return (value-reminder)/100;
+  var reminder = value % 100;
+  return (value - reminder) / 100;
 }
 console.log(howManyHundreds(89));
